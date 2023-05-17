cce86fd4cee3aabfa3222add3a056294b2cf1bca
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-05-17 21:22:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+const output = document.querySelector('#output');
+
+const randomTime = Math.floor(Math.random * (5 - 1 + 1) + 1);
+
+const timeout = function (ms) {
+  return new Promise((resolve) => setTimeout(resolve(`this is ${ms}`), ms * 1000));
+};
+
+
+for (let i = 0; i < 5; i++) {
+	promises.push(timeout.bind(randomTime));
+}
+
+const callIt = aysnc () => {
+	const first = await Promise.any(...promises);
+	output.innerText = first;
+}
+
+
